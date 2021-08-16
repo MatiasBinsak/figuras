7d9f8ab5e8097a73b4b24a1bbc83086dde37da79
MatiasBinsak <matiasbinsak@gmail.com>
2021-08-16 20:13:34 -0300

    Se agregar contructor y variables

---
+++ src/figuras/Rectangulo.java
@@ -0,0 +1,13 @@
+package figuras;
+
+public class Rectangulo {
+
+	
+	private double ancho;
+	private double largo;
+
+	public Rectangulo(double ancho, double largo) {
+	this.ancho= ancho;
+	this.largo= largo;
+}
+}
