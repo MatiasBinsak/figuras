d8a23cbf9cb7831d29d31b8e3e52dd86c5e76e34
MatiasBinsak <matiasbinsak@gmail.com>
2021-08-16 21:08:15 -0300

    Se agrega clase cuadrado

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -2,12 +2,11 @@ package figuras;
 
 public class Rectangulo {
 
-	
 	private double ancho;
 	private double largo;
 
 	public Rectangulo(double ancho, double largo) {
-	this.ancho= ancho;
-	this.largo= largo;
-}
+		this.ancho = ancho;
+		this.largo = largo;
+	}
 }
